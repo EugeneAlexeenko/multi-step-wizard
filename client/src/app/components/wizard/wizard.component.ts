d74f48ea86300448205e76a6c8e4583dc89c0b7d
Evgenii Alekseenko <eugene.alexeenko@gmail.com>
2024-07-03 15:32:54 +0200

    feat(client): handle config downloading error

--- client/src/app/components/wizard/wizard.component.ts
+++ client/src/app/components/wizard/wizard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { WizardService } from '../../services/wizard.service';
 import { WizardStep } from '../../models/wizard-step.model';
 
@@ -12,13 +14,21 @@ import { WizardStep } from '../../models/wizard-step.model';
 })
 export class WizardComponent implements OnInit {
   steps: WizardStep[] = [];
+  error: string | null = null;
 
   constructor(private wizardService: WizardService) { }
 
   ngOnInit(): void {
-    this.wizardService.getWizardSteps().subscribe(steps => {
-      this.steps = steps;
-    });
+    this.wizardService.getWizardSteps().pipe(
+      catchError(error => {
+        this.error = 'Error loading wizard steps';
+        console.error('Error loading wizard steps:', error);
+        return of([])
+      })
+    ).
+      subscribe(steps => {
+        this.steps = steps;
+      });
   }
 
 }
